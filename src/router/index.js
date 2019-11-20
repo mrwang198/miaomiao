976cf1524e028ac94c677182aad79453d0bad724
wangxiaosen <2644767982@qq.com>
2019-11-20 19:01:30 +0800

    第二次提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,33 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-
+import movieRouter from "./movie";
+import mineRouter from "./mine";
+import cinemaRouter from "./cinema";
 Vue.use(VueRouter);
 
 const routes = [
+  movieRouter,
+  mineRouter,
+  cinemaRouter,
+  //路由重定向
   {
-    path: "/",
-    name: "home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/*",
+    redirect: "/movie"
   }
+  // {
+  //   path: "/",
+  //   name: "home",
+  //   component: Home
+  // },
+  // {
+  //   path: "/about",
+  //   name: "about",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
+  // }
 ];
 
 const router = new VueRouter({
